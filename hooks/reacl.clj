6fb4c611510067df2b1d479bcd6b913267060327
Marcus Crestani <marcus.crestani@active-group.de>
2023-06-22 14:14:23 +0200

    Fix local-state binding.

--- hooks/reacl.clj
+++ hooks/reacl.clj
@@ -15,7 +15,7 @@
         app-state (if (empty? (:children ?app-state)) ?app-state nil)
         bindings (if (empty? (:children ?app-state)) ?bindings ?app-state)
         clauses (if (empty? (:children ?app-state)) ?clauses (concat [?bindings] ?clauses))
-        local-state (get (into {} (mapv vec (partition 2 clauses))) (api/token-node 'local-state))
+        local-state (get (into {} (mapv (fn [[k v]] [(pr-str k) v]) (partition 2 clauses))) (pr-str (api/token-node 'local-state)))
         new-node
         (api/list-node
          (list* (api/token-node 'do)
